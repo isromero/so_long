4722f9bf10987430e9a221c6443ea4dffacf1612
isromero <ismaromeroortega@gmail.com>
2023-04-06 17:29:24 +0200

    no leaks because mlx_string_put deleted, so no movements in screen, just terminal

--- handle_and_windows.c
+++ handle_and_windows.c
@@ -3,33 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   handle_and_windows.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isromero <isromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: isromero <isromero@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 22:32:46 by isromero          #+#    #+#             */
-/*   Updated: 2023/03/23 15:39:59 by isromero         ###   ########.fr       */
+/*   Updated: 2023/04/06 17:28:39 by isromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-void	ft_mov_display(t_data *data, t_img *img)
-{
-    size_t i;
-    i = 0;
-
-    img->mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/empty.xpm", &img->img_width, &img->img_height);
-    while (i < (32 * data->map_width))
-    {
-        mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img->mlx_img, i, data->map_height * 32);
-        i += 32;
-    }
-   	mlx_string_put(data->mlx_ptr, data->win_ptr,
-		data->map_width / 2, data->map_height * 34, 0xff0000,
-		"MOVEMENTS ");
-    mlx_string_put(data->mlx_ptr, data->win_ptr,
-		data->map_width * 30, data->map_height * 34, 0xff0000,
-		ft_itoa(data->player_mov));
-}
 
 int	handle_keypress(int key, t_data *data)
 {
@@ -59,7 +40,7 @@ void    creating_window(int key, t_data *data, t_img *img)
     int height = 0;
     height = data->map_height * 32;
     data->mlx_ptr = mlx_init();
-    data->win_ptr = mlx_new_window(data->mlx_ptr, width, height + 32, "so_long");
+    data->win_ptr = mlx_new_window(data->mlx_ptr, width, height, "so_long");
     img->mlx_img = mlx_new_image(data->mlx_ptr, width, height); //crea una imagen en la memoria de video de la pantalla
     img->addr = mlx_get_data_addr(img->mlx_img, &img->bpp, &img->line_len, &img->endian); //se devuelve un puntero al primer byte de la imagen donde se usa para escribir en ella pixel por pixel
 	init_points(data);
--- movements.c
+++ movements.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   movements.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isromero <isromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: isromero <isromero@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 22:29:58 by isromero          #+#    #+#             */
-/*   Updated: 2023/03/23 16:57:32 by isromero         ###   ########.fr       */
+/*   Updated: 2023/04/06 17:28:04 by isromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,15 @@ void    move_up(int key, t_data *data)
         data->player_y--;
         img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "img/initial.xpm", &img.img_width, &img.img_height);
        	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->player_x * 32, data->player_y * 32);
+		mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "img/empty.xpm", &img.img_width, &img.img_height);
         mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->player_x * 32, (data->player_y + 1) * 32);
+		mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         if (data->map[data->player_y + 1][data->player_x] == 'E')
         {
             img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "img/exit_closed.xpm", &img.img_width, &img.img_height);
             mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->player_x * 32, (data->player_y + 1) * 32);
+			mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         }
      }
 }
@@ -41,12 +44,15 @@ void    move_left(int key, t_data *data)
         data->player_x--;
         img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/initial.xpm", &img.img_width, &img.img_height);
         mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->player_x * 32, data->player_y * 32);
+		mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/empty.xpm", &img.img_width, &img.img_height);
         mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, (data->player_x + 1) * 32, data->player_y * 32);
+		mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         if(data->map[data->player_y][data->player_x + 1] == 'E')
         {
             img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/exit_closed.xpm", &img.img_width, &img.img_height);
             mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, (data->player_x + 1) * 32, data->player_y * 32);
+			mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         }
     }
 }
@@ -59,12 +65,15 @@ void    move_down(int key, t_data *data)
         data->player_y++;
         img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/initial.xpm", &img.img_width, &img.img_height);
         mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->player_x * 32, data->player_y * 32);
+		mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/empty.xpm", &img.img_width, &img.img_height);
-            mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->player_x * 32, (data->player_y - 1) * 32);
+        mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->player_x * 32, (data->player_y - 1) * 32);
+		mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         if(data->map[data->player_y - 1][data->player_x] == 'E')
         {
             img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/exit_closed.xpm", &img.img_width, &img.img_height);
             mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->player_x * 32, (data->player_y - 1) * 32);
+			mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         }
     }
 }
@@ -78,12 +87,15 @@ void    move_right(int key, t_data *data)
         data->player_x++;
         img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/initial.xpm", &img.img_width, &img.img_height);
         mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->player_x * 32, data->player_y * 32);
+		mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/empty.xpm", &img.img_width, &img.img_height);
         mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, (data->player_x - 1) * 32, data->player_y * 32);
+		mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         if(data->map[data->player_y][data->player_x - 1] == 'E')
         {
             img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/exit_closed.xpm", &img.img_width, &img.img_height);
             mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, (data->player_x - 1) * 32, data->player_y * 32);
+			mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         }
     }
 }
@@ -93,6 +105,7 @@ void    ft_move_initial(int key, t_data *data)
 	t_img img;
     img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "img/initial.xpm", &img.img_width, &img.img_height);
     mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->player_x * 32, data->player_y * 32);
+	mlx_destroy_image(data->mlx_ptr, img.mlx_img);
 
     move_up(key, data);
     move_left(key, data);
@@ -107,14 +120,15 @@ void    ft_move_initial(int key, t_data *data)
     {
 		img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/empty.xpm", &img.img_width, &img.img_height);
         mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->door_x * 32, data->door_y * 32);
+		mlx_destroy_image(data->mlx_ptr, img.mlx_img);
         img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, "./img/exit_open.xpm", &img.img_width, &img.img_height);
         mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img.mlx_img, data->door_x * 32, data->door_y * 32);
+		mlx_destroy_image(data->mlx_ptr, img.mlx_img);
 		if(data->map[data->player_y][data->player_x] == 'E') //Con esto solucionas el no pasar por arriba de la puerta hasta q no se colecione todo
         {
             ft_printf("%s\n", "YOU ARE SAFE!");
             exit(0);
        	}
     }
-    ft_mov_display(data, &img);
     ft_printf("Movements: %d\n", data->player_mov);
 }
